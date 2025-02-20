44430de2b5685bbf1e84a20d56d0d39aae072b81
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-20 22:15:55 +0000

    Formatation for printing added (exercice 6)

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -8,6 +8,10 @@ namespace ManyStrings
     {
         private static void Main(string[] args)
         {
+            // var for excercice 6
+            double xx = 1.23456;
+            int ii = 19;
+
             // Var string for exercice 4
             string stringTest1 = "\'";
             string stringTest2 = "\\";
@@ -29,15 +33,19 @@ namespace ManyStrings
             string loveMessage = $"I {heartIcon} my GF";
             string stringTest10 = $"My favorite ice cream flavour is {aux}";
 
+            // priting strings for exercice 4
             Console.WriteLine($"{stringTest1}\n{stringTest2}\n{stringTest3}");
             Console.WriteLine($"{stringTest4}\n{stringTest5}\n{stringTest6}");
             Console.WriteLine($"{stringTest7}");
-
+            
+            // priting strings for exercice 5
             Console.WriteLine($"{stringTest8}\n{stringTest9}"); 
-
             Console.WriteLine(loveMessage);
             Console.WriteLine(stringTest10);
 
+            // priting var for exercice 6
+            Console.WriteLine($"{xx:f2}\n{xx:p1}");
+            Console.WriteLine($"{ii:X}\n{ii:C}"); 
         }
     }
 }
