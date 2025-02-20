55a191029681562d39c164174762057b2fb84544
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-20 22:23:11 +0000

    Added new project and ask for interger value

---
+++ AskForInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Ask for a interger number
+            Console.WriteLine("Insert a interger number: ");
+            int valueInserted1 = int.Parse(Console.ReadLine());
+        }
+    }
+}
