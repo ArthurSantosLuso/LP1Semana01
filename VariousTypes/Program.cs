85e84d336ff0866d51e96d27ede011aa9b5b374e
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-20 21:14:36 +0000

    Different real variables declaration, initialization and printing added

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -6,6 +6,7 @@ namespace VariousTypes
     {
         private static void Main(string[] args)
         {
+            // Variables declaration area
             int interger = 5;
             long longInterger = 1000000;
             short shortInterger = 50;
@@ -13,9 +14,18 @@ namespace VariousTypes
 
             char charUniCode1 = '\u00A9', charUniCode2 = '\u00B1', charUniCode3 = '\u02AC';
 
+            float floatVarExample = 1.1f;
+            double doubleVarExample = 5.55;
+            decimal decimalVarExample = 8.566M; 
+
+            // Printing int's variables
             Console.WriteLine($"{interger}\n{longInterger}\n{shortInterger}\n{oneByteInterger}");
 
+            // Printing char's variables
             Console.WriteLine($"{charUniCode1}\n{charUniCode2}\n{charUniCode3}");
+
+            // Printing real's variables
+            Console.WriteLine($"{floatVarExample}\n{doubleVarExample}\n{decimalVarExample}");
 
 
         }
