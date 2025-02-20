2b9d351167b9e06d70466e486d23cb32dbb6d2d1
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-20 20:16:52 +0000

    VariousType project added & creation and initialization of int variables

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int interger = 5;
+            long longInterger = 1000000;
+            short shortInterger = 50;
+            sbyte oneByteInterger = 12;
+
+            Console.WriteLine($"{interger}");
+            Console.WriteLine($"{longInterger}");
+            Console.WriteLine($"{shortInterger}");
+            Console.WriteLine($"{oneByteInterger}");
+        }
+    }
+}
