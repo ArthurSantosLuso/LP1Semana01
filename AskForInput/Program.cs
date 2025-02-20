af947ad04b3a291471257118efc4d86101339914
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-20 22:24:58 +0000

    Printing sum of both input values added

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -10,8 +10,13 @@ namespace AskForInput
             // Ask for a interger number
             Console.WriteLine("Insert a interger number: ");
             int valueInserted1 = int.Parse(Console.ReadLine());
+
+            // Ask for a real number
             Console.WriteLine("Insert a real number: ");
             float valueInserted2 = float.Parse(Console.ReadLine());
+            
+            // Print the adding of both inputs
+            Console.WriteLine($"{valueInserted1 + valueInserted2}");
 
         }
     }
