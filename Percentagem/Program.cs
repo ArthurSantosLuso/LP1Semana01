d9c91bac4a0b0d5ac01326bccdeadb2e250b725b
Arthur Santos <arthurluiz.developer@gmail.com>
2025-02-21 14:14:48 +0000

    Exercicio de avaliação Resolvido

---
+++ Percentagem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            float valor1 = float.Parse(Console.ReadLine());
+            float valor2 = float.Parse(Console.ReadLine());
+            float valor3 = float.Parse(Console.ReadLine()); 
+            float valor4 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{valor1:P2}\n{valor2:P2}\n{valor3:P2}\n{valor4:P2}");
+        }
+    }
+}
